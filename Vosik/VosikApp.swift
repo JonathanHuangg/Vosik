88f5054defa3b41571704740ebc1cbef6d82ed41
Jonathan Huang <jonathannhuang@gmail.com>
2024-02-10 10:56:53 -0500

    Initial Commit

---
+++ Vosik/VosikApp.swift
@@ -0,0 +1,17 @@
+//
+//  VosikApp.swift
+//  Vosik
+//
+//  Created by Jonathan Huang on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VosikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
