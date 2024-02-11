98d076faf5ec4396305cf6a96b6dc7d3e923beb2
Mason Rein <masonarein@gmail.com>
2024-02-11 04:24:05 -0500

    Added Spotify API

---
+++ Vosik-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Vosik-Bridging-Header.h
+//  Vosik
+//
+//  Created by Mason Rein on 2/11/24.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
---
+++ Vosik/Music MP3/Constants.swift
@@ -0,0 +1,39 @@
+//
+//  Constants.swift
+//  Vosik
+//
+//  Created by Mason Rein on 2/11/24.
+//
+
+import Foundation
+
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"vosik://")! // You will add your redirectURI.
+let spotifyClientId = "6562412d9da042d9a3ea741b7e680a2f"
+let spotifyClientSecretKey = "176aeb8a77c249d88fc6c2dca46145c2"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
